79e859da95966cb3918b0083ac5af924aa573883
Alex Wykoff <alex@alexwykoff.com>
2021-03-30 16:52:46 -0500

    updating footer links

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -9,10 +9,17 @@ const Footer: React.FC = () => {
       <Grid>
         <Row>
           <Col xs={12} sm={6}>
-            <h3>Have questions? Text us.</h3>
+            <h3>Learn More</h3>
+            <p><a href="https://dogecoin.org/gettingstarted">Getting Started</a></p>
+            <p><a href="https://www.reddit.com/r/dogeducation">Dogeducation Subreddit</a></p>
+            <p><a href="https://github.com/dogecoin/dogecoin">Dogecoin Github</a></p>
           </Col>
           <Col xs={12} sm={6}>
-            <h3>Follow us</h3>
+            <h3>Community & Support</h3>
+            <p><a href="https://twitter.com/Dogecoin">Twitter</a></p>
+            <p><a href="https://www.reddit.com/r/dogecoin">Reddit</a></p>
+            <p><a href="https://discord.gg/dogecoin">Discord</a></p>
+            <p><a href="https://facebook.com/OfficialDogecoin">Facebook</a></p>
           </Col>
         </Row>
       </Grid>
