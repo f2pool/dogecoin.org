57254cc2d27797c9fe7b63baa1cf0c42b1f4ff9c
Alex Wykoff <alex@alexwykoff.com>
2021-03-30 14:17:06 -0500

    adding initial copy for explanation section and updated some hashtags

--- src/containers/SectionAbout/index.tsx
+++ src/containers/SectionAbout/index.tsx
@@ -8,23 +8,25 @@ import { useFormatMessages } from "../../utils/hooks"
 import * as S from "./styled"
 
 const hashtags = [
-  { text: "fun", color: "#00665E", bg: "#FFD203" },
-  { text: "bonk", color: "#FFF", bg: "#00BEB5" },
+  { text: "muchfunny", color: "#00665E", bg: "#FFD203" },
+  { text: "dogecoinarmy", color: "#FFF", bg: "#00BEB5" },
   { text: "cryptocurrency", color: "#232D91", bg: "#C3F1FF" },
-  { text: "unique", color: "#FFF", bg: "#00BEB5" },
+  { text: "suchmemes", color: "#FFF", bg: "#00BEB5" },
   { text: "doge", color: "#232D91", bg: "#FFF" },
   { text: "shiba", color: "#00665E", bg: "#FFD203" },
 ]
 
 const SectionAbout: React.FC = () => {
-  const [title, text] = useFormatMessages([{ id: "ABOUT_TITLE" }, { id: "ABOUT_TEXT" }])
+  const [title, text1, text2, text3] = useFormatMessages([{ id: "ABOUT_TITLE" }, { id: "ABOUT_TEXT_1" }, { id: "ABOUT_TEXT_2" }, { id: "ABOUT_TEXT_3" }])
 
   return (
     <Section as={S.Wrapper}>
       <Row center="xs">
         <Col xs={12} sm={8}>
           <S.Title>{title}</S.Title>
-          <S.Text>{text}</S.Text>
+          <S.Text>{text1}</S.Text>
+          <S.Text>{text2}</S.Text>
+          <S.Text>{text3}</S.Text>
 
           <S.ImageStack></S.ImageStack>
 
