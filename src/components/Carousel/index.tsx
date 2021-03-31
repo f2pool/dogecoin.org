ec295b11c5f3ed7044538c8ab34645951ac17946
vince19972 <vince19972@gmail.com>
2021-03-31 19:25:46 -0400

    [sectionAbout] revise carousel content

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -10,15 +10,14 @@ import * as S from "./styled"
 /*
  * Constants
  */
-// TODO: use real data
 const slides = [
   {
     staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/dip.png"} alt={alt} />,
     link: "#",
   },
   {
-    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/lion-king.png"} alt={alt} />,
-    link: "https://twitter.com/elonmusk/status/1357236825589432322",
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/mavericks.png"} alt={alt} />,
+    link: "https://twitter.com/mcuban/status/1368276718860046339",
   },
   {
     staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/soon.png"} alt={alt} />,
@@ -33,24 +32,40 @@ const slides = [
     link: "https://www.coindesk.com/teslas-elon-musk-wants-coinbase-to-become-doge-friendly",
   },
   {
-    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/mavericks.png"} alt={alt} />,
-    link: "https://twitter.com/mcuban/status/1368276718860046339",
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/lion-king.png"} alt={alt} />,
+    link: "https://twitter.com/elonmusk/status/1357236825589432322",
   },
   {
     staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/finish.png"} alt={alt} />,
     link: "https://www.digitaltrends.com/social-media/dogecoin-jamaican-bobsled-team-olympics/",
   },
   {
-      staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/ethpeople.png"} alt={alt} />,
-      link: "#",
-  },
-  {
     staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/racecar.png"} alt={alt} />,
     link: "https://ftw.usatoday.com/2014/04/nascar-doge-dogecoin-dogecar-josh-wise",
   },
   {
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/manifesto.png"} alt={alt} />,
+    link: "#",
+  },
+  {
     staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/moon.png"} alt={alt} />,
     link: "https://www.vice.com/en/article/kbzbxx/dogecoin-is-going-to-the-moon-for-real",
+  },
+  {
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/needyou.png"} alt={alt} />,
+    link: "#",
+  },
+  {
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/joan.png"} alt={alt} />,
+    link: "#",
+  },
+  {
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/dogeus.png"} alt={alt} />,
+    link: "https://www.reuters.com/article/us-crypto-currency-ethereum-dogecoin-idUSKBN2A417H",
+  },
+  {
+    staticImg: (alt: string) => <StaticImage src={"../../assets/images/memes/dogeto.png"} alt={alt} />,
+    link: "https://www.coindesk.com/dogecoin-price-surges-37-following-binance-listing-announcement",
   },
 ]
 
@@ -59,13 +74,20 @@ const Carousel: React.FC = () => {
   const handleVisibilityChange = (isVisible: boolean): void => setPageIsVisible(isVisible)
   const alts: string[] = useFormatMessages([
     { id: "MEME_DIP_ALT" },
-    { id: "MEME_LIONKING_ALT" },
+    { id: "MEME_MAVS_ALT" },
     { id: "MEME_SOON_ALT" },
     { id: "MEME_CYBERTRUCK_ALT" },
-    { id: "MEME_MAVS_ALT" },
+    { id: "MEME_COINBASE_ALT" },
+    { id: "MEME_LIONKING_ALT" },
     { id: "MEME_FINISH_ALT" },
     { id: "MEME_RACECAR_ALT" },
+    { id: "MEME_MANIFESTO_ALT" },
+    { id: "MEME_SAKURA_ALT" },
     { id: "MEME_MOON_ALT" },
+    { id: "MEME_NEEDYOU_ALT" },
+    { id: "MEME_JOAN_ALT" },
+    { id: "MEME_DOGEUS_ALT" },
+    { id: "MEME_DOGETO_ALT" },
   ])
 
   return (
