d745e4c9ba9e1404a2403f3d97e27f01f8d97e1e
vince19972 <vince19972@gmail.com>
2021-03-31 11:09:09 -0400

    [CardStep] extract component

--- src/containers/SectionGuide/index.tsx
+++ src/containers/SectionGuide/index.tsx
@@ -5,76 +5,18 @@ import { useFormatMessages } from "../../utils/hooks"
 
 import Section from "../../components/Section"
 import Button from "../../components/Button"
-
-import { IconCart, IconWallet, IconDownload, IconKey } from "../../icons"
-
-import Oval from "../../assets/shapes/oval"
+import CardStep from "../../components/CardStep"
 
 import * as S from "./styled"
 
 const Shapes: React.FC = () => {
-  const [
-    stepOneSubtitle,
-    stepOneTitle,
-    stepTwoSubtitle,
-    stepTwoTitle,
-    stepThreeSubtitle,
-    stepThreeTitle,
-    stepFourSubtitle,
-    stepFourTitle,
-  ] = useFormatMessages([
-    { id: "GUIDE_STEP_1_SUBTITLE" },
-    { id: "GUIDE_STEP_1_TITLE" },
-    { id: "GUIDE_STEP_2_SUBTITLE" },
-    { id: "GUIDE_STEP_2_TITLE" },
-    { id: "GUIDE_STEP_3_SUBTITLE" },
-    { id: "GUIDE_STEP_3_TITLE" },
-    { id: "GUIDE_STEP_4_SUBTITLE" },
-    { id: "GUIDE_STEP_4_TITLE" },
-  ])
-  const stepsData = [
-    {
-      subtitle: stepOneSubtitle,
-      title: stepOneTitle,
-      shape: <Oval $type={2} />,
-      icon: <IconKey />,
-    },
-    {
-      subtitle: stepTwoSubtitle,
-      title: stepTwoTitle,
-      shape: <Oval $type={2} $rotate={90} />,
-      icon: <IconWallet />,
-    },
-    {
-      subtitle: stepThreeSubtitle,
-      title: stepThreeTitle,
-      shape: <Oval $rotate={145} />,
-      icon: <IconCart />,
-    },
-    {
-      subtitle: stepFourSubtitle,
-      title: stepFourTitle,
-      shape: <Oval $type={3} $rotate={330} />,
-      icon: <IconDownload />,
-    },
-  ]
-
   return (
     <>
-      {stepsData.map((step, index) => {
-        return (
-          <S.StepCol xs={12} sm={3} key={index}>
-            <S.StepShape>
-              {step.shape}
-              <S.Icon>{step.icon}</S.Icon>
-            </S.StepShape>
-            <S.StepTexts>
-              <p>{step.subtitle}</p>
-              <h3>{step.title}</h3>
-            </S.StepTexts>
-          </S.StepCol>
-        )
-      })}
+      {[1, 2, 3, 4].map((step) => (
+        <S.StepCol xs={12} sm={3} key={step}>
+          <CardStep step={step} />
+        </S.StepCol>
+      ))}
     </>
   )
 }
--- src/containers/SectionGuide/styled.tsx
+++ src/containers/SectionGuide/styled.tsx
@@ -14,65 +14,7 @@ export const HeroRow = styled(Row)`
   margin-bottom: ${rem(24)};
 `
 
-export const StepShape = styled.div`
-  position: relative;
-  width: ${rem(144)};
-  height: ${rem(144)};
-  margin: 0 auto;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  > svg {
-    max-width: 100%;
-    max-height: 100%;
-    position: relative;
-  }
-
-  @media all and (max-width: ${(props) => `${props.theme.flexboxgrid.breakpoints.md}em`}) {
-    flex-direction: column;
-  }
-`
-
-export const StepTexts = styled.div`
-  text-align: center;
-
-  p {
-    font-weight: ${(props) => props.theme.fontWeight.medium};
-    color: ${(props) => props.theme.colors.dark};
-    opacity: 0.4;
-    margin-bottom: ${rem(15)};
-    font-size: ${rem(17)};
-    line-height: 1;
-  }
-
-  h3 {
-    font-size: ${rem(32)};
-    line-height: ${rem(36)};
-    font-weight: ${(props) => props.theme.fontWeight.medium};
-    color: ${(props) => props.theme.colors.primary};
-    margin: 0 auto;
-    max-width: ${rem(200)};
-  }
-
-  @media all and (max-width: ${(props) => `${props.theme.flexboxgrid.breakpoints.md}em`}) {
-    p {
-      margin-bottom: ${rem(0)};
-    }
-  }
-`
-
 export const StepCol = styled(Col)`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-
-  ${StepShape} {
-    margin-bottom: ${rem(24)};
-  }
-
   @media all and (max-width: ${(props) => `${props.theme.flexboxgrid.breakpoints.md}em`}) {
     &:not(:last-child) {
       margin-bottom: ${rem(56)};
@@ -90,16 +32,4 @@ export const ShapesRow = styled(Row)`
 
 export const Wrapper = styled.section`
   padding: ${rem(120)} 0;
-`
-
-export const Icon = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-
-  svg {
-    width: ${rem(40)};
-    margin-right: 0;
-  }
 `
