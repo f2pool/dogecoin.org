c07e2bf6f5561bc92b8b7f445e402d216bd36afc
Balaj Marius <balajmarius@gmail.com>
2021-11-26 11:14:50 +0200

    dogecoin movie setup

--- src/components/Main.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-const Main = styled.main`
-  min-height: 100vh;
-  overflow: hidden;
-`
-
-export default Main
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  mode: "jit",
+  plugins: [],
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false,
+  variants: {},
+  theme: {
+    extend: {
+      colors: {
+        black: "#0f0604",
+        yellow: "#f1f462",
+      },
+      backgroundImage: {
+        main: "url('../img/bg.png')",
+      },
+      width: {
+        104: "28rem",
+        "1/2": "32rem",
+        "1/4": "24rem",
+        "1/6": "16rem",
+      },
+      height: {
+        "1/2": "32rem",
+        "1/4": "24rem",
+        "1/6": "16rem",
+      },
+      fontFamily: {
+        display: ["Tulpen One", "sans-serif"],
+        body: ["Big Shoulders Display", "sans-serif"],
+      },
+      textColor: (theme) => ({
+        yellow: theme("colors.yellow"),
+      }),
+      stroke: (theme) => ({
+        yellow: theme("colors.yellow"),
+      }),
+      fill: (theme) => ({
+        yellow: theme("colors.yellow"),
+      }),
+    },
+  },
+};
